d88f6f233198e2aee426c4eeb98f1efce2828277
Robsonmendes1987 <robsonmendes1987@hotmail.com>
2024-06-19 11:34:59 -0300

    feat(utils): adicionando RetornaCpfOuCnpj para determinar o tipo de documento (CPF ou CNPJ) com base no número fornecido, utilizando métodos auxiliares ValidarCpf.isValidCPF e ValidarCnpj.isValidCNPJ

---
+++ src/main/java/com/example/cross_border_service/utils/RetornaCpfOuCnpj.java
@@ -0,0 +1,14 @@
+package com.example.cross_border_service.utils;
+
+public class RetornaCpfOuCnpj {
+    public static String validarCpfOuCnpj(String numero) {
+        String cleaned = numero.replaceAll("\\D", "");
+        if (cleaned.length() == 11 && ValidarCpf.isValidCPF(cleaned)) {
+            return "Física";
+        } else if (cleaned.length() == 14 && ValidarCnpj.isValidCNPJ(cleaned)) {
+            return "Jurídica";
+        } else {
+            return "Não Informado";
+        }
+    }
+}
