3349892d98a14bb4d4f40519206e62ab4254ad88
Robsonmendes1987 <robsonmendes1987@hotmail.com>
2024-06-19 10:52:27 -0300

    feat(config): adicionando  OpenApiConfig Adiciona a configuração do Swagger usando Springdoc para agrupar endpoints da API REST grupo user-api com caminhos correspondentes a /api/**.

---
+++ src/main/java/com/example/cross_border_service/config/OpenApiConfig.java
@@ -0,0 +1,19 @@
+package com.example.cross_border_service.config;
+
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+
+    @Bean
+    public GroupedOpenApi userApi() {
+        return GroupedOpenApi.builder()
+                .group("user-api")
+                .pathsToMatch("/api/**")
+                .build();
+    }
+
+}
