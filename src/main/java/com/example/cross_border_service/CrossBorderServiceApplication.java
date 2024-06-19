8c9c55e2d77c6fd6fd6e183c88c10536f055e629
Robsonmendes1987 <robsonmendes1987@hotmail.com>
2024-06-19 07:58:51 -0300

    Initial commit

---
+++ src/main/java/com/example/cross_border_service/CrossBorderServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.cross_border_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrossBorderServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrossBorderServiceApplication.class, args);
+	}
+
+}
