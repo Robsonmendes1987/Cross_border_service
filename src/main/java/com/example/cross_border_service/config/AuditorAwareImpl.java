aedd27045d30785438c7a27ad0af26ef5d8877f0
Robsonmendes1987 <robsonmendes1987@hotmail.com>
2024-06-19 10:48:16 -0300

    feat(config): adicionando  AuditorAwareImpl fornecer informações sobre quem está realizando uma operação de auditoria no banco de dados

---
+++ src/main/java/com/example/cross_border_service/config/AuditorAwareImpl.java
@@ -0,0 +1,15 @@
+package com.example.cross_border_service.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
+
+import java.util.Optional;
+
+@Configuration
+public class AuditorAwareImpl implements AuditorAware<String> {
+
+    @Override
+    public Optional<String> getCurrentAuditor() {
+        return Optional.empty();
+    }
+}
