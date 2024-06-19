77daee6c44a32db8e5a68a5586d2b47800e0b530
Robsonmendes1987 <robsonmendes1987@hotmail.com>
2024-06-19 11:30:14 -0300

    feat(utils): adicionando CapitalizeNames para normalizar a capitalização de nomes, mantendo preposições em minúsculas, exceto quando aparecem no início do nome

---
+++ src/main/java/com/example/cross_border_service/utils/CapitalizeNames.java
@@ -0,0 +1,27 @@
+package com.example.cross_border_service.utils;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+public class CapitalizeNames {
+    private static final Set<String> prepositions = new HashSet<>(Arrays.asList("da", "de", "do", "das", "dos", "e"));
+
+    public static String capitalizeName(String name) {
+        String[] words = name.toLowerCase().split("\\s+");
+        StringBuilder capitalized = new StringBuilder();
+
+        for (int i = 0; i < words.length; i++) {
+            if (prepositions.contains(words[i]) && i != 0) {
+                capitalized.append(words[i]);
+            } else {
+                capitalized.append(Character.toUpperCase(words[i].charAt(0)))
+                        .append(words[i].substring(1));
+            }
+            if (i < words.length - 1) {
+                capitalized.append(" ");
+            }
+        }
+        return capitalized.toString();
+    }
+}
