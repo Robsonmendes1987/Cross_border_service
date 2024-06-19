5909a2f5e18ff1d323ed19ed2cbc0eb77c96bc76
Robsonmendes1987 <robsonmendes1987@hotmail.com>
2024-06-19 16:45:51 -0300

    correçoes e ajustes no arquivo resources, acesso e swagger

--- src/main/java/com/example/cross_border_service/model/repository/WalletRepository.java
+++ src/main/java/com/example/cross_border_service/model/repository/WalletRepository.java
@@ -1,6 +1,5 @@
 package com.example.cross_border_service.model.repository;
-
-import com.inter.desafio.model.entity.WalletEntity;
+import com.example.cross_border_service.model.entity.WalletEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface WalletRepository extends JpaRepository<WalletEntity, Long>{
