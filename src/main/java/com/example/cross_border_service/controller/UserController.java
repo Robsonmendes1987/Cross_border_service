2da9b46fb9b51ba7413d335c25c1cea4d1c5f4cc
Robsonmendes1987 <robsonmendes1987@hotmail.com>
2024-06-19 11:53:50 -0300

    feat(controller): adicionando UserController  define endpoints RESTful para manipular operações CRUD relacionadas aos usuários da  aplicação

---
+++ src/main/java/com/example/cross_border_service/controller/UserController.java
@@ -0,0 +1,61 @@
+package com.example.cross_border_service.controller;
+
+import com.example.cross_border_service.model.dto.ResponseUserDTO;
+import com.example.cross_border_service.model.dto.RequestUserDTO;
+import com.example.cross_border_service.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+@RestController
+@RequestMapping("/api/v1/user")
+@Tag(name = "User API", description = "End point´s do cadastro de usuarios disponíveis na aplicação.")
+
+public class UserController {
+    private final UserService userService;
+
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/")
+    @Operation(summary = "Lista todos os usuários cadastrados")
+    public ResponseEntity<List<ResponseUserDTO>> getAll() {
+        return ResponseEntity.ok().body( userService.getAll() );
+    }
+
+    @GetMapping("/{id}")
+    @Operation(summary = "Retorna o usuário que o id seja igual a requisição.")
+    public ResponseEntity<ResponseUserDTO> getById( @PathVariable Long id ) {
+        return ResponseEntity.ok().body(userService.getById(id));
+    }
+
+    @GetMapping("/nome/{nome}")
+    @Operation(summary = "Lista usuários que contém o mesmo nome no banco de dados")
+    public ResponseEntity<List<ResponseUserDTO>> getByName( @PathVariable String nome ) {
+        return ResponseEntity.ok().body(userService.getByName(nome));
+    }
+
+    @PostMapping("/")
+    @Operation(summary = "Salva um novo usuário no banco de dados")
+    public ResponseEntity<ResponseUserDTO> saveUser(@RequestBody RequestUserDTO user) {
+        return ResponseEntity.ok().body(userService.saveUser(user));
+    }
+
+    @PutMapping("/id/{id}")
+    @Operation(summary = "Atualiza o cadastro do usuário")
+    public ResponseEntity<ResponseUserDTO> updateUserById(@Valid @PathVariable Long id, @RequestBody RequestUserDTO request) {
+        return ResponseEntity.ok().body(userService.updateUserById(id, request));
+    }
+
+    @DeleteMapping("/id/{id}")
+    @Operation(summary = "Apaga definitivamente um usuário no banco de dados")
+    public ResponseEntity<String> deleteUserById(@PathVariable Long id) {
+        return ResponseEntity.ok().body(userService.deleteUserById(id));
+    }
+
+}
