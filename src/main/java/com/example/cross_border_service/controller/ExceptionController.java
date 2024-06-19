0fa22db9c6e208f9ef1f94dc9f8add05ba96186b
Robsonmendes1987 <robsonmendes1987@hotmail.com>
2024-06-19 11:49:42 -0300

    feat(controller): adicionando ExceptionController centraliza o tratamento de diferentes tipos de exceções que podem surgir na aplicação, fornecendo respostas HTTP adequadas e estruturadas com informações detalhadas sobre os erros encontrados

---
+++ src/main/java/com/example/cross_border_service/controller/ExceptionController.java
@@ -0,0 +1,115 @@
+package com.example.cross_border_service.controller;
+import com.example.cross_border_service.exception.DuplicateValue;
+import com.example.cross_border_service.exception.NotFound;
+import com.example.cross_border_service.exception.StandardError;
+import jakarta.servlet.ServletRequest;
+import jakarta.validation.ConstraintViolationException;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
+
+@ControllerAdvice
+public class ExceptionController {
+    @ExceptionHandler(NotFound.class)
+    public ResponseEntity<StandardError> notFoundException(NotFound e, ServletRequest request) {
+        StandardError error = new StandardError (
+                System.currentTimeMillis(),
+                HttpStatus.NOT_FOUND.value(),
+                e.getMessage()
+        );
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
+    }
+
+    @ExceptionHandler(DuplicateValue.class)
+    public ResponseEntity<StandardError> integratyViolationException(DuplicateValue e, ServletRequest request) {
+        StandardError error = new StandardError(
+                System.currentTimeMillis(),
+                HttpStatus.BAD_REQUEST.value(),
+                e.getMessage()
+        );
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST.value()).body(error);
+    }
+
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Object> handleValidationExceptions(MethodArgumentNotValidException ex, WebRequest request) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+
+        ErrorResponse errorResponse = new ErrorResponse(
+                LocalDateTime.now(),
+                HttpStatus.BAD_REQUEST.value(),
+                "Erro de validação",
+                errors,
+                request.getDescription(false)
+        );
+
+        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<Object> handleConstraintViolationException(ConstraintViolationException ex, WebRequest request) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getConstraintViolations().forEach(violation -> {
+            String fieldName = violation.getPropertyPath().toString();
+            String errorMessage = violation.getMessage();
+            errors.put(fieldName, errorMessage);
+        });
+
+        ErrorResponse errorResponse = new ErrorResponse(
+                LocalDateTime.now(),
+                HttpStatus.BAD_REQUEST.value(),
+                "Erro de validação",
+                errors,
+                request.getDescription(false)
+        );
+
+        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<Object> handleGlobalException(Exception ex, WebRequest request) {
+        ErrorResponse errorResponse = new ErrorResponse(
+                LocalDateTime.now(),
+                HttpStatus.INTERNAL_SERVER_ERROR.value(),
+                ex.getMessage(),
+                null,
+                request.getDescription(false)
+        );
+
+        return new ResponseEntity<>(errorResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @Getter
+    @Setter
+    static class ErrorResponse {
+        private LocalDateTime timestamp;
+        private int status;
+        private String error;
+        private Object message;
+        private String path;
+
+        public ErrorResponse(LocalDateTime timestamp, int status, String error, Object message, String path) {
+            this.timestamp = timestamp;
+            this.status = status;
+            this.error = error;
+            this.message = message;
+            this.path = path;
+        }
+
+    }
+}
