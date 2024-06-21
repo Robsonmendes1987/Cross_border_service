e8cee07b8994d1f8a9ca00f4a6c69c9caf3e7beb
Robsonmendes1987 <robsonmendes1987@hotmail.com>
2024-06-20 22:17:09 -0300

    feat(controller): adicionando Classe Transactionntroller gerencia requisições relacionadas às transações entre usuários dentro da API .

--- src/main/java/com/example/cross_border_service/controller/TransactionController.java
+++ src/main/java/com/example/cross_border_service/controller/TransactionController.java
@@ -1,4 +1,33 @@
 package com.example.cross_border_service.controller;
 
+import com.example.cross_border_service.model.dto.Transaction.RequestTransactionDTO;
+import com.example.cross_border_service.model.dto.Transaction.ResponseTransactionDTO;
+import com.example.cross_border_service.service.CotacaoService;
+import com.example.cross_border_service.service.TransactionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/transaction")
 public class TransactionController {
+
+    @Autowired
+    private TransactionService transactionService;
+
+    @Autowired
+    private CotacaoService cotacaoService;
+
+    @PostMapping("/transferir")
+    public String transferir(@RequestBody RequestTransactionDTO transaction) {
+        return transactionService.transferir(transaction);
+    }
+
+    @GetMapping("/listar/{usuarioId}")
+    public ResponseEntity<List<ResponseTrasactionDTO>> listarTransacoesPorUsuario(@PathVariable Long usuarioId) {
+        return ResponseEntity.ok().body(transactionService.listarTransacoesPorUsuario(usuarioId));
+    }
 }
