30092917d45a1076f14c81b1227839787dc17d64
Robsonmendes1987 <robsonmendes1987@hotmail.com>
2024-06-20 21:52:06 -0300

    feat(repository): adicionando Classe CotacaoRepository fornece métodos para realizar operações de persistência na entidade CotacaoEntity.

---
+++ src/main/java/com/example/cross_border_service/model/repository/CotacaoRepository.java
@@ -0,0 +1,14 @@
+package com.example.cross_border_service.model.repository;
+import com.example.cross_border_service.model.entity.CotaçaoEntity;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.time.LocalDate;
+import java.util.Optional;
+
+public interface CotacaoRepository extends JpaRepository<CotacaoEntity, Long> {
+
+    Optional<CotacaoEntity> findByDataCotacao(LocalDate dataCotacao);
+
+}
